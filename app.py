a14e5148b29d5f2bbcffe62a58fd8512dd523543
UtsavAdhikari3 <utsavadhikari99@gmail.com>
2025-07-01 17:42:05 +0545

    number guessing game

---
+++ app.py
@@ -0,0 +1,64 @@
+import random
+
+def generate_random_num():
+    return random.randrange(1,101)
+
+   
+
+
+def guess_num(choice):
+    guess_num = int(input("Enter your guess: "))
+    random_number = generate_random_num()
+
+    if choice == 1:
+        for i in range(10):
+            if guess_num == random_number:
+                print(f"Congratulations! You guessed the correct number in {i} attempts.")
+            elif guess_num > random_number:
+                print(f"Incorrect! The number is less than {guess_num}.")
+            elif guess_num < random_number:
+                print(f"Incorrect! The number is greater than {guess_num}.")
+    elif choice == 2:
+        for i in range(5):
+            if guess_num == random_number:
+                print(f"Congratulations! You guessed the correct number in {i} attempts.")
+            elif guess_num > random_number:
+                print(f"Incorrect! The number is less than {guess_num}.")
+            elif guess_num < random_number:
+                print(f"Incorrect! The number is greater than {guess_num}.")
+    elif choice == 3:
+        for i in range(3):
+            if guess_num == random_number:
+                print(f"Congratulations! You guessed the correct number in {i} attempts.")
+            elif guess_num > random_number:
+                print(f"Incorrect! The number is less than {guess_num}.")
+            elif guess_num < random_number:
+                print(f"Incorrect! The number is greater than {guess_num}.")
+    else:
+        print("Sorry but your choice is out of bounds.")
+def main():
+    print("""Welcome to the Number Guessing Game!
+I'm thinking of a number between 1 and 100.
+You have 5 chances to guess the correct number.
+          """)
+    
+    print("""Please select the difficulty level:
+1. Easy (10 chances)
+2. Medium (5 chances)
+3. Hard (3 chances)""")
+    
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        print("""Great! You have selected the Easy difficulty level.
+Let's start the game!""")
+        guess_num(choice)
+    elif choice == 2:
+        print("""Great! You have selected the Medium difficulty level.
+Let's start the game!""")
+        guess_num(choice)
+    
+    else:
+        print("""Great! You have selected the Hard difficulty level.
+Let's start the game!""")
+        guess_num(choice)
